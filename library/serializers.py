40afa08159d3714d1d3fcd24e1e05f2a13199821
Aswathi-k15 <158160693+Aswathi-k15@users.noreply.github.com>
2024-12-17 12:34:29 +0530

    Update serializers.py

--- library/serializers.py
+++ library/serializers.py
@@ -1,8 +1,14 @@
 # serializers.py
 from rest_framework import serializers
 from .models import LibraryHistory
+from student.models import *
 
 class LibraryHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = LibraryHistory
         fields = '_all_'
+        
+class StudentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['student_id', 'name', 'age', 'phone_number', 'class_name', 'address']
