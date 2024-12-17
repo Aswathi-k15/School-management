19bc1e193d09ba4ba9bdba4fba40f38ba5d43f51
Aswathi-k15 <158160693+Aswathi-k15@users.noreply.github.com>
2024-12-17 18:53:24 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student', 'library', 'officeStaff', 
+    'admin', 'rest_framework', 
 ]
 
 MIDDLEWARE = [
